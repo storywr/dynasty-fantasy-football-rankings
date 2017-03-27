4802bbcc896a750751874b166f9b784c309c8081
Bill <storywr@gmail.com>
2017-03-27 17:29:53 -0400

    added update positional ranking feature

--- client/src/components/PlayersShow.js
+++ client/src/components/PlayersShow.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import * as ReactBootstrap from 'react-bootstrap';
 import { PageHeader } from 'react-bootstrap';
 
-const PlayersShow = (props) => {
-  const player = props.player;
-  const comments = props.comments.filter(comment => comment.player_id === player.id)
+class PlayersShow extends Component {
 
-  return (
-    <div>
-      <PageHeader>{player.name} <small>{player.team}</small></PageHeader>
-      <img src={player.pic}/><br></br><br></br>
-      <p>Position: {player.position}</p>
-      <p>Positional Ranking: {player.positional_ranking}</p>
-      <h5>Comments:</h5>
-      <ul>{comments.map(comment =>
-        <li>{comment.summary}</li>
-      )}</ul>
-      <Link to={`/players/${player.id}/comments/new`}>Add Comment</Link>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: props.player
+    };
+  }
+
+  handleOnClick(event) {
+    this.state.player.positional_ranking--
+    this.setState({
+      player: this.state.player
+    })
+  }
+
+  render() {
+    const player = this.state.player;
+    const comments = this.props.comments.filter(comment => comment.player_id === player.id)
+
+    return (
+      <div>
+        <PageHeader>{player.name} <small>{player.team}</small></PageHeader>
+        <img src={player.pic}/><br></br><br></br>
+        <p>Position: {player.position}</p>
+        <p>Positional Ranking: {player.positional_ranking}</p>
+        <h5>Comments:</h5>
+        <ul>{comments.map(comment =>
+          <li>{comment.summary}</li>
+        )}</ul>
+        <Link to={`/players/${player.id}/comments/new`}>Add Comment</Link>
+        <button onClick={(event) => this.handleOnClick(event)} type="button">Lower Positional Rank</button>
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state, ownProps) => {
