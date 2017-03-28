34c5084890bc72d75bfde1dd267a378debf0989b
Bill <storywr@gmail.com>
2017-03-28 17:55:34 -0400

    added adp data to rb

--- client/src/components/RunningBacks.js
+++ client/src/components/RunningBacks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { PageHeader, Carousel } from 'react-bootstrap';
+import '../Positions.css'
 
 class RunningBacks extends Component {
 
@@ -10,17 +11,66 @@ class RunningBacks extends Component {
     this.state = {
       players: props.players.sort(function(a, b){
         return a.positional_ranking - b.positional_ranking
-      })
+      }),
+      mflplayers: props.mflplayers,
+      adp: props.adp
     };
   }
 
   render() {
     const players = this.state.players;
+    const mflplayers = this.state.mflplayers;
+    const adp = this.state.adp;
+
+    var hash = Object.create(null);
+
+    mflplayers.concat(adp).forEach(function(obj) {
+        hash[obj.id] = Object.assign(hash[obj.id] || {}, obj);
+    });
+
+    var a3 = Object.keys(hash).map(function(key) {
+        return hash[key];
+    });
+
+    a3 = a3.filter(player => player.averagePick > 0)
+
+    a3.sort(function(a, b){
+      return a.averagePick - b.averagePick
+    })
+
+    a3.forEach(player => {
+      var fullName = player.name
+      fullName = fullName.split(',');
+      var lastName = fullName[0]
+      var firstName = fullName[1]
+      player.name = firstName.concat(" ")
+      player.name = player.name.concat(lastName)
+    })
+
+    function checkMflRB(player) {
+      return player.position === "RB"
+    }
+
+    a3 = a3.filter(checkMflRB)
+
+    const topRB = a3.slice(0, 40)
+
     return (
       <div>
-        <PageHeader>Running Back Rankings <small>The Return of the Workhorse</small></PageHeader>
-        <img src={"https://static01.nyt.com/images/2015/10/16/sports/16FALCONSweb2/16FALCONSweb2-master1050.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
-          <Carousel>
+        <PageHeader className="header1">Running Back Rankings <small>The Return of the Workhorse</small></PageHeader>
+        <PageHeader className="header2">ADP <small>Check the Market</small></PageHeader>
+        <div className="positionpic">
+          <img src={"https://static01.nyt.com/images/2015/10/16/sports/16FALCONSweb2/16FALCONSweb2-master1050.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
+        </div>
+        <div className="adpdata">
+          <tr>
+            <th><ol>{topRB.map(player =>
+              <li>{player.name}</li>
+            )}</ol></th>
+          </tr>
+        </div>
+        <div className="players">
+          <Carousel className="carousel">
             {players.map(player =>
               <Carousel.Item>
                 <img width={100} height={135} alt="500x150" src={player.pic}/>
@@ -30,6 +80,7 @@ class RunningBacks extends Component {
               </Carousel.Item>
             )}
           </Carousel>
+        </div>
       </div>
     );
   };
@@ -41,7 +92,9 @@ function checkRB(player) {
 
 const mapStateToProps = (state) => {
   return {
-    players: state.players.filter(checkRB)
+    players: state.players.filter(checkRB),
+    mflplayers: state.mflplayers.players.player,
+    adp: state.adp.adp.player
   };
 };
 
