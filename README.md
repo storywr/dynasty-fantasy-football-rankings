9e6bd9fa6ab04b616f60de853601b2e019c39124
Bill <storywr@gmail.com>
2017-03-28 02:20:14 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 bundle && cd client && npm i && cd ..
+
 bundle exec rake db:create db:migrate db:seed
+
+cd ..
+
 rake start
--- client/src/actions/adp.js
+++ client/src/actions/adp.js
@@ -1,6 +1,6 @@
 export const fetchMyFantasyLeagueADP = () => {
   return dispatch => {
-    return fetch('http://www77.myfantasyleague.com/2017/export?TYPE=adp&L=18474&W=&JSON=1&IS_KEEPER=1')
+    return fetch('http://www77.myfantasyleague.com/2017/export?TYPE=adp&L=18474&W=&JSON=1')
       .then(response => response.json())
       .then(adp => dispatch({ type: 'LOAD_MFL_ADP_SUCCESS', adp }))
       .catch(console.log)
