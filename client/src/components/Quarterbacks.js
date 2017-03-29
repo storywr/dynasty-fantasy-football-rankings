f0eedb9f95bfd12e7b7e0110b5d76cebaf07ad65
Bill <storywr@gmail.com>
2017-03-29 00:04:14 -0400

    added adp to all positional pages

--- client/src/components/Quarterbacks.js
+++ client/src/components/Quarterbacks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { PageHeader, Carousel } from 'react-bootstrap';
+import '../Positions.css'
 
 class Quarterbacks extends Component {
 
@@ -10,17 +11,66 @@ class Quarterbacks extends Component {
     this.state = {
       players: props.players.sort(function(a, b){
         return a.positional_ranking - b.positional_ranking
-      })
+      }),
+      mflplayers: props.mflplayers,
+      adp: props.adp
     };
   }
 
   render() {
     const players = this.state.players;
+    const mflplayers = this.state.mflplayers;
+    const adp = this.state.adp;
+
+    var hash = Object.create(null);
+
+    mflplayers.concat(adp).forEach(function(obj) {
+        hash[obj.id] = Object.assign(hash[obj.id] || {}, obj);
+    });
+
+    var a3 = Object.keys(hash).map(function(key) {
+        return hash[key];
+    });
+
+    a3 = a3.filter(player => player.averagePick > 0)
+
+    a3.sort(function(a, b){
+      return a.averagePick - b.averagePick
+    })
+
+    a3.forEach(player => {
+      var fullName = player.name
+      fullName = fullName.split(',');
+      var lastName = fullName[0]
+      var firstName = fullName[1]
+      player.name = firstName.concat(" ")
+      player.name = player.name.concat(lastName)
+    })
+
+    function checkMflQB(player) {
+      return player.position === "QB"
+    }
+
+    a3 = a3.filter(checkMflQB)
+
+    const topQB = a3.slice(0, 40)
+
     return (
       <div>
-        <PageHeader>Quarterback Rankings <small>Going Deep</small></PageHeader>
-        <img src={"https://static01.nyt.com/images/2015/01/20/sports/20patriots1/20patriots1-master1050.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
-          <Carousel>
+        <PageHeader className="header1">Quarterback Rankings <small>The Return of the Workhorse</small></PageHeader>
+        <PageHeader className="header2">ADP <small>Check the Market</small></PageHeader>
+        <div className="positionpic">
+          <img src={"https://static01.nyt.com/images/2015/01/20/sports/20patriots1/20patriots1-master1050.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
+        </div>
+        <div className="adpdata">
+          <tr>
+            <th><ol>{topQB.map(player =>
+              <li>{player.name}</li>
+            )}</ol></th>
+          </tr>
+        </div>
+        <div className="players">
+          <Carousel className="carousel">
             {players.map(player =>
               <Carousel.Item>
                 <img width={100} height={135} alt="500x150" src={player.pic}/>
@@ -30,6 +80,7 @@ class Quarterbacks extends Component {
               </Carousel.Item>
             )}
           </Carousel>
+        </div>
       </div>
     );
   };
@@ -41,7 +92,9 @@ function checkQB(player) {
 
 const mapStateToProps = (state) => {
   return {
-    players: state.players.filter(checkQB)
+    players: state.players.filter(checkQB),
+    mflplayers: state.mflplayers.players.player,
+    adp: state.adp.adp.player
   };
 };
 
--- client/src/components/WideReceivers.js
+++ client/src/components/WideReceivers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { PageHeader, Carousel } from 'react-bootstrap';
+import '../Positions.css'
 
 class WideReceivers extends Component {
 
@@ -10,17 +11,66 @@ class WideReceivers extends Component {
     this.state = {
       players: props.players.sort(function(a, b){
         return a.positional_ranking - b.positional_ranking
-      })
+      }),
+      mflplayers: props.mflplayers,
+      adp: props.adp
     };
   }
 
   render() {
     const players = this.state.players;
+    const mflplayers = this.state.mflplayers;
+    const adp = this.state.adp;
+
+    var hash = Object.create(null);
+
+    mflplayers.concat(adp).forEach(function(obj) {
+        hash[obj.id] = Object.assign(hash[obj.id] || {}, obj);
+    });
+
+    var a3 = Object.keys(hash).map(function(key) {
+        return hash[key];
+    });
+
+    a3 = a3.filter(player => player.averagePick > 0)
+
+    a3.sort(function(a, b){
+      return a.averagePick - b.averagePick
+    })
+
+    a3.forEach(player => {
+      var fullName = player.name
+      fullName = fullName.split(',');
+      var lastName = fullName[0]
+      var firstName = fullName[1]
+      player.name = firstName.concat(" ")
+      player.name = player.name.concat(lastName)
+    })
+
+    function checkMflWR(player) {
+      return player.position === "WR"
+    }
+
+    a3 = a3.filter(checkMflWR)
+
+    const topWR = a3.slice(0, 40)
+
     return (
       <div>
-        <PageHeader>Wide Receiver Rankings <small>Straight Cash Homie</small></PageHeader>
-        <img src={"https://s-media-cache-ak0.pinimg.com/originals/0d/34/ea/0d34ea2a4e192454162711f3e548cc22.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
-          <Carousel>
+        <PageHeader className="header1">Wide Receiver Rankings <small>The Return of the Workhorse</small></PageHeader>
+        <PageHeader className="header2">ADP <small>Check the Market</small></PageHeader>
+        <div className="positionpic">
+          <img src={"https://s-media-cache-ak0.pinimg.com/originals/0d/34/ea/0d34ea2a4e192454162711f3e548cc22.jpg"} style={{height: '600', width: '900px', margin: 'auto'}}/><br></br><br></br>
+        </div>
+        <div className="adpdata">
+          <tr>
+            <th><ol>{topWR.map(player =>
+              <li>{player.name}</li>
+            )}</ol></th>
+          </tr>
+        </div>
+        <div className="players">
+          <Carousel className="carousel">
             {players.map(player =>
               <Carousel.Item>
                 <img width={100} height={135} alt="500x150" src={player.pic}/>
@@ -30,6 +80,7 @@ class WideReceivers extends Component {
               </Carousel.Item>
             )}
           </Carousel>
+        </div>
       </div>
     );
   };
@@ -41,7 +92,9 @@ function checkWR(player) {
 
 const mapStateToProps = (state) => {
   return {
-    players: state.players.filter(checkWR)
+    players: state.players.filter(checkWR),
+    mflplayers: state.mflplayers.players.player,
+    adp: state.adp.adp.player
   };
 };
 
