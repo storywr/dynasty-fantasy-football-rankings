9645236a0f2fa44c5f94aa8c44ff324e543c67f5
Bill <storywr@gmail.com>
2017-04-06 18:10:18 -0400

    rosters now working

--- client/src/components/RosterShow.js
+++ client/src/components/RosterShow.js
@@ -8,11 +8,17 @@ import '../App.css'
 class RosterShow extends Component {
 
   render() {
+    const mflplayers = this.props.mflplayers;
+    const rosterplayers = this.props.roster.player;
+
+    rosterplayers.forEach(player => {
+      player.name = mflplayers.find(mflplayer => mflplayer.id == player.id)
+    })
 
     return (
       <div>
-        <ul>{this.props.roster.player.map(player =>
-          <li>{player.id}</li>
+        <ul>{rosterplayers.map(player =>
+          <li>{player.name.name}</li>
         )}</ul>
       </div>
     );
@@ -21,6 +27,7 @@ class RosterShow extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    mflplayers: state.mflplayers.players.player,
     roster: state.rosters.rosters.franchise.find(roster => roster.id == ownProps.routeParams.id)
   };
 };
