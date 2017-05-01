b6f6fd6354ae62bebe4c8188e85991149da9e276
Bill <storywr@gmail.com>
2017-05-01 17:27:21 -0400

    organized seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# WR
 Player.create({name: "Odell Beckham", position: "WR", team: "NY Giants", positional_ranking: 1, pic: "http://www.rotoworld.com/images/headshots/NFL/9403.jpg"})
 Player.create({name: "A.J. Green", position: "WR", team: "Cincinnati Bengals", positional_ranking: 8, pic: "http://www.rotoworld.com/images/headshots/NFL/6438.jpg"})
 Player.create({name: "Mike Evans", position: "WR", team: "Tampa Bay Buccaneers", positional_ranking: 2, pic: "http://www.rotoworld.com/images/headshots/NFL/9296.jpg"})
@@ -9,6 +10,17 @@ Player.create({name: "DeAndre Hopkins", position: "WR", team: "Houston Texans", 
 Player.create({name: "T.Y. Hilton", position: "WR", team: "Indianapolis Colts", positional_ranking: 9, pic: "http://www.rotoworld.com/images/headshots/NFL/7558.jpg"})
 Player.create({name: "Michael Thomas", position: "WR", team: "New Orleans Saints", positional_ranking: 10, pic: "http://www.rotoworld.com/images/headshots/NFL/11222.jpg"})
 
+Comment.create({summary: "OBJ is a freak of nature. I've never seen a player pull off some of the circus catches that he has. He is in a tier of his own and at the young age of 24, he should stay there for a long time.", player_id: 1})
+Comment.create({summary: "Biggest playmaker to hit the league since Deion Sanders", player_id: 1})
+Comment.create({summary: "He's become overrated in the fantasy community. If I have the first pick in a startup I'm going with Zeke", player_id: 1})
+Comment.create({summary: "Passed OBJ for the top spot this season. He is the Tampa Bay offense.", player_id: 3})
+Comment.create({summary: "Evans has looked like Megatron 2.0 for the Bucs.", player_id: 3})
+
+
+
+
+
+# RB
 Player.create({name: "Ezekiel Elliott", position: "RB", team: "Dallas Cowboys", positional_ranking: 1, pic: "http://www.rotoworld.com/images/headshots/NFL/11265.jpg"})
 Player.create({name: "Le'Veon Bell", position: "RB", team: "Pittsburgh Steelers", positional_ranking: 2, pic: "http://www.rotoworld.com/images/headshots/NFL/8390.jpg"})
 Player.create({name: "David Johnson", position: "RB", team: "Arizona Cardinals", positional_ranking: 3, pic: "http://www.rotoworld.com/images/headshots/NFL/10404.jpg"})
@@ -20,6 +32,20 @@ Player.create({name: "Isaiah Crowell", position: "RB", team: "Cleveland Browns",
 Player.create({name: "Carlos Hyde", position: "RB", team: "San Francisco 49ers", positional_ranking: 9, pic: "http://www.rotoworld.com/images/headshots/NFL/9381.jpg"})
 Player.create({name: "Lamar Miller", position: "RB", team: "Houston Texans", positional_ranking: 10, pic: "http://www.rotoworld.com/images/headshots/NFL/7408.jpg"})
 
+Comment.create({summary: "Zeke burst onto the scene as the most exciting rookie RB since Adrian Peterson. Only 22 and rushing behind the Dallas o-line, he's my top dynasty asset for the forseeable future.", player_id: 11})
+Comment.create({summary: "Barry Sanders 2.0.", player_id: 14})
+Comment.create({summary: "Changing the game with his patient running style.", player_id: 12})
+Comment.create({summary: "Almost went for 1000+ yards rushing and receiver. He's the most dominant fantasy asset since LT.", player_id: 13})
+Comment.create({summary: "Broke Matt Forte's rushing record in only 14 games. The kid is the real deal.", player_id: 15})
+Comment.create({summary: "Down year due to the Rams offense, but his talent can't be argued.", player_id: 16})
+Comment.create({summary: "Injury concerns but he's the most violent runner since Marshawn Lynch.", player_id: 17})
+Comment.create({summary: "Severely underrated behind that o-line.", player_id: 18})
+
+
+
+
+
+# QB
 Player.create({name: "Tom Brady", position: "QB", team: "New England Patriots", positional_ranking: 1, pic: "http://www.rotoworld.com/images/headshots/NFL/1163.jpg"})
 Player.create({name: "Aaron Rodgers", position: "QB", team: "Green Bay Packers", positional_ranking: 2, pic: "http://www.rotoworld.com/images/headshots/NFL/3118.jpg"})
 Player.create({name: "Andrew Luck", position: "QB", team: "Indianapolis Colts", positional_ranking: 3, pic: "http://www.rotoworld.com/images/headshots/NFL/6439.jpg"})
@@ -31,6 +57,17 @@ Player.create({name: "Derek Carr", position: "QB", team: "Oakland Raiders", posi
 Player.create({name: "Matt Ryan", position: "QB", team: "Atlanta Falcons", positional_ranking: 9, pic: "http://www.rotoworld.com/images/headshots/NFL/4637.jpg"})
 Player.create({name: "Drew Brees", position: "QB", team: "New Orleans Saints", positional_ranking: 10, pic: "http://www.rotoworld.com/images/headshots/NFL/591.jpg"})
 
+Comment.create({summary: "T-Mobile really showed he's elite this last year. Top 10 QB for sure.", player_id: 27})
+Comment.create({summary: "The GOAT. Enough said.", player_id: 21})
+Comment.create({summary: "He's the most dominant QB in fantasy for the foreseeable future.", player_id: 22})
+Comment.create({summary: "The future at the QB position.", player_id: 23})
+Comment.create({summary: "He's a gunslinger. Took the Skins from a mediocre offense to the number 3 passing offense in the league.", player_id: 24})
+
+
+
+
+
+#TE
 Player.create({name: "Rob Gronkowski", position: "TE", team: "New England Patriots", positional_ranking: 1, pic: "http://www.rotoworld.com/images/headshots/NFL/5729.jpg"})
 Player.create({name: "Travis Kelce", position: "TE", team: "Kansas City Chiefs", positional_ranking: 7, pic: "http://www.rotoworld.com/images/headshots/NFL/8411.jpg"})
 Player.create({name: "Tyler Eifert", position: "TE", team: "Cincinnati Bengals", positional_ranking: 3, pic: "http://www.rotoworld.com/images/headshots/NFL/8408.jpg"})
@@ -42,23 +79,5 @@ Player.create({name: "Jimmy Graham", position: "TE", team: "Seattle Seahawks", p
 Player.create({name: "Zach Ertz", position: "TE", team: "Philadelphia Eagles", positional_ranking: 9, pic: "http://www.rotoworld.com/images/headshots/NFL/8409.jpg"})
 Player.create({name: "Delanie Walker", position: "TE", team: "Tennessee Titans", positional_ranking: 10, pic: "http://www.rotoworld.com/images/headshots/NFL/3976.jpg"})
 
-Comment.create({summary: "OBJ is a freak of nature. I've never seen a player pull off some of the circus catches that he has. He is in a tier of his own and at the young age of 24, he should stay there for a long time.", player_id: 1})
-Comment.create({summary: "Biggest playmaker to hit the league since Deion Sanders", player_id: 1})
-Comment.create({summary: "He's become overrated in the fantasy community. If I have the first pick in a startup I'm going with Zeke", player_id: 1})
-Comment.create({summary: "Passed OBJ for the top spot this season. He is the Tampa Bay offense.", player_id: 3})
-Comment.create({summary: "Evans has looked like Megatron 2.0 for the Bucs.", player_id: 3})
-Comment.create({summary: "Zeke burst onto the scene as the most exciting rookie RB since Adrian Peterson. Only 22 and rushing behind the Dallas o-line, he's my top dynasty asset for the forseeable future.", player_id: 11})
-Comment.create({summary: "T-Mobile really showed he's elite this last year. Top 10 QB for sure.", player_id: 27})
-Comment.create({summary: "Barry Sanders 2.0.", player_id: 14})
-Comment.create({summary: "Changing the game with his patient running style.", player_id: 12})
-Comment.create({summary: "Almost went for 1000+ yards rushing and receiver. He's the most dominant fantasy asset since LT.", player_id: 13})
-Comment.create({summary: "Broke Matt Forte's rushing record in only 14 games. The kid is the real deal.", player_id: 15})
-Comment.create({summary: "Down year due to the Rams offense, but his talent can't be argued.", player_id: 16})
-Comment.create({summary: "Injury concerns but he's the most violent runner since Marshawn Lynch.", player_id: 17})
-Comment.create({summary: "Severely underrated behind that o-line.", player_id: 18})
-Comment.create({summary: "The GOAT. Enough said.", player_id: 21})
-Comment.create({summary: "He's the most dominant QB in fantasy for the foreseeable future.", player_id: 22})
-Comment.create({summary: "The future at the QB position.", player_id: 23})
-Comment.create({summary: "He's a gunslinger. Took the Skins from a mediocre offense to the number 3 passing offense in the league.", player_id: 24})
 Comment.create({summary: "When he's healthy there's really no competition. Gronk has changed the TE position.", player_id: 31})
 Comment.create({summary: "Not quite as talented as Gronk but less injury concerns and far more consistent.", player_id: 32})
